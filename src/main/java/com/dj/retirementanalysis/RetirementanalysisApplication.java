c3df9c0721cfa12b7b5fd82fbdc8e511ec4cd6cf
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-09-11 11:46:30 +0200

    first commit

---
+++ src/main/java/com/dj/retirementanalysis/RetirementanalysisApplication.java
@@ -0,0 +1,13 @@
+package com.dj.retirementanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetirementanalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetirementanalysisApplication.class, args);
+	}
+
+}
